a8dc50ede83d27fcdc2f97622dfdd6e7452063cc
Ivan <ivansortino2003@gmail.com>
2023-07-04 23:50:06 -0300

    Dia Seis

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class xTarjet extends HTMLElement{
         this.parrafo = this.getAttribute("parrafo");
         this.repo = this.getAttribute("repo");
         this.pag = this.getAttribute("pag")
-        
+        this.boton = this.getAttribute("boton-cerrar")
         this.render();
     }
     
@@ -15,14 +15,14 @@ class xTarjet extends HTMLElement{
             position: absolute;
             left:32.5%;
             z-index:1;
-            width:35%;
-            height:35%;
             border: 2px solid white;
             border-radius: 10px;
             color: #EFEFFB;
             background-color: #6c757d;
-            
-            animation: entrada 1s;
+            animation: entrada 0.7s;
+            width: 100vh;
+            max-width: 600px;
+            margin-top: 100px;
         }
         @keyframes entrada{
             from{
@@ -41,7 +41,7 @@ class xTarjet extends HTMLElement{
             align-items: center;
         }
         .cuadrado-hijo h2{
-            margin-top: 25px;
+            
             font-size: 3.2rem; 
         }
         .cuadrado-hijo p{
@@ -51,6 +51,16 @@ class xTarjet extends HTMLElement{
         }
         .boton-Iz{
             margin-right: 20px;
+        }
+        .boton-Iz:hover{
+            transition: 0.5s;
+            background-color: white;
+            color: #495057;
+        }
+        .boton-Dr:hover{
+            transition: 0.5s;
+            background-color: white;
+            color: #495057;
         }
         .boton-Dr,.boton-Iz{
             background-color: #495057;
@@ -69,23 +79,30 @@ class xTarjet extends HTMLElement{
             cursor: pointer;
             padding: 13px 14px;
             margin-top: 10px;
+            margin-bottom:10px;
             border-radius: 5px;
         }
+        .cerrar:hover{
+            transition: 0.5s;
+            background-color: white;
+            color: #495057;
+        }
+        
         `
     }
     render() {
        
         this.innerHTML = ` 
         <style>${this.style()}</style>
-        <div class='cuadrado-padre'>
+        <div class='cuadrado-padre ' >
             <div class='cuadrado-hijo'>
                 <h2>${this.Titulo}</h2>
 			    <p>${this.parrafo}</p>
                 <div>
-                    <a href="${this.repo}"><button class="boton-Iz">Link del Repositorio</button></a>
-                    <a href="${this.pag}"><button class="boton-Dr">Link de la Pagina </button></a>
+                    <a href="${this.repo}" target="_blank"><button class="boton-Iz">Link del Repositorio</button></a>
+                    <a href="${this.pag}" target="_blank"><button class="boton-Dr">Link de la Pagina </button></a>
                 </div>
-			    <button onclick="cerrar();" class='cerrar'>Cerrar</button>
+			    <button onclick="${this.boton}" class='cerrar' >Cerrar</button>
 		    </div>
             
         </div>
@@ -95,4 +112,63 @@ class xTarjet extends HTMLElement{
     
 }
 
-customElements.define("tarjeta-proy", xTarjet)
+customElements.define("tarjeta-proy", xTarjet);
+
+
+
+// ABRIR CERRAR BMW
+function mostrarBm(){
+    document.querySelector('.bm-no').style.display = 'block';   
+    document.querySelector('.abc').classList.add('oscuro');
+}
+function ocultarBm(){
+    document.querySelector('.bm-no').style.display = 'none';
+    document.querySelector('.abc').classList.remove('oscuro');
+    
+}
+// ABRIR CERRAR BMW
+
+// ABRIR CERRAR FUNTPETS
+function mostrarPets(){
+    document.querySelector('.pets-no').style.display = 'block';
+    document.querySelector('.abc').classList.add('oscuro');
+}
+function ocultarPets(){
+    document.querySelector('.pets-no').style.display = 'none';
+    document.querySelector('.abc').classList.remove('oscuro');
+}
+// ABRIR CERRAR FUNTPETS
+
+// ABRIR CERRAR GLOBETROTTER
+function mostrarGlobre(){
+    document.querySelector('.globre-no').style.display = 'block';
+    document.querySelector('.abc').classList.add('oscuro');
+}
+function ocultarGlobre(){
+    document.querySelector('.globre-no').style.display = 'none';
+    document.querySelector('.abc').classList.remove('oscuro');
+}
+// ABRIR CERRAR GLOBETROTTER
+
+// ABRIR CERRAR PRESENTACION
+function mostrarPres(){
+    document.querySelector('.pres-no').style.display = 'block';
+    document.querySelector('.abc').classList.add('oscuro');
+}
+function ocultarPres(){
+    document.querySelector('.pres-no').style.display = 'none';
+    document.querySelector('.abc').classList.remove('oscuro');
+}
+// ABRIR CERRAR PRESENTACION
+
+// ABRIR CERRAR NORWAY
+function mostrarNorway(){
+    document.querySelector('.norway-no').style.display = 'block';
+    document.querySelector('.abc').classList.add('oscuro');
+}
+function ocultarNorway(){
+    document.querySelector('.norway-no').style.display = 'none';
+    document.querySelector('.abc').classList.remove('oscuro');
+}
+// ABRIR CERRAR NORWAY
+
